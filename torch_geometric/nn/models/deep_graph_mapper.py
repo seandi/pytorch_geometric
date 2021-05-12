cc1c9aab712aa1d3f196519cba6b03cb36c768b6
seandi <eandi.simo@outlook.it>
2021-05-12 18:50:41 +0200

    Add documentation

--- torch_geometric/nn/models/deep_graph_mapper.py
+++ torch_geometric/nn/models/deep_graph_mapper.py
@@ -17,8 +17,45 @@ from torch_geometric.utils import to_networkx
 
 
 class DGM:
-    """
-    DGM
+    r"""
+    The Deep Graph Mapper module for graph visualization from the `"Deep Graph Mapper: Seeing Graphs through the
+    Neural Lens" <https://arxiv.org/abs/2002.03864>`_ paper. This implementation is based on the authors' `official
+    implementation <https://github.com/crisbodnar/dgm>`_.
+    Given an input graph, DGM maps it to a new graph that compactly represents the original one using the Mapper
+    algorithm combined with a given trainable lens function (refer to section 3.1 of the DGM
+    `paper <https://arxiv.org/abs/2002.03864>`_ for an overview of Mapper). The mapped graph is further processed
+    to filter out components that are too small and then is visualized.
+
+    Args:
+        lens_function (Data -> ndarray):
+            The lens functions that takes as input the :obj:`Data` object representing the graph to visualize and
+            produces the node features to be used for the visualization.
+        num_intervals (int):
+            The number of intervals over the :math:`[0,1]` interval to be used for generating the visualization.
+            More intervals results in more fine-grained visualizations of the graph.
+        overlap (float):
+            The ratio of overlap between two consecutive intervals, expressed as a value between
+            :math:`0` (disjoint intervals) and :math:`1` (fully overlapping).
+            More overlap between intervals results in higher connectivity of the visualized graph.
+        embedding_dims (int):
+            The dimension of the node features/embeddings used for creating the visualized graph.
+            If the lens function produces nodes embeddings with higher dimensions than this value,
+            a dimensionality reduction algorithm is applied on top. Should be either :math:`1` or :math:`2`.
+        embedding_reduction_method (str, optional):
+            The dimensionality algorithm to be used if the lens function is high dimensional. (default: :obj:`"tsne"`)
+        reduction_random_state (int, optional):
+            The random initial state of the dimensionality reduction algorithm for reproducible results.
+            (default: :obj:`0`).
+        min_component_size (int, optional):
+            The minimum size that a connected component of the mapped graph should have to not be
+            filtered out in the visualized graph. The size of the component is given by the sum of
+            the size of its nodes, each of which is given byb the number of nodes in the connected
+            components of the original graph that has been mapped to that node. (default: :obj:`0`)
+        use_sdgm (bool, optional):
+            Flag to use Structural Deep Graph Mapper, instead of DGM. This changes how the edges of
+            the mapped graph are constructed. See Appendix B of the DGM `paper <https://arxiv.org/abs/2002.03864>`_.
+            (default: :obj:`False`)
+
     """
 
     _AVAILABLE_REDUCTION_METHODS = ['tsne', 'pca', 'isomap']
@@ -54,7 +91,12 @@ class DGM:
 
     def _make_intervals(self, low: float = 0.0, high: float = 1.0
                         ) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+        """
+        Generates self.num_intervals equally sized intervals in the [0,1] range such that each two consecutive
+        intervals have an overlap that is a self.overlap factor of the length of their union.
+        """
 
+        # Find the interval length satisfying the constraints.
         x = (high - low) / (self.num_intervals - self.overlap * self.num_intervals + self.overlap)
 
         intervals_low = np.linspace(start=low, stop=high - x, num=self.num_intervals).reshape((-1, 1))
@@ -91,18 +133,28 @@ class DGM:
         r"""
 
         Args:
-            data:
-            use_true_labels:
-            figsize:
-            node_size_scale:
-            edge_width_scale:
-            legend_dict:
-            discrete_colormap:
-            save_dir:
-            name:
-
-        Returns:
-
+            data (Data):
+                The :obj:`Data` object representing the input graph.
+            use_true_labels (bool, optional):
+                Set this flag to :obj:`True` to use the nodes true labels (if available) to determine the color of
+                the nodes in the visualization instead of using the (reduced) node embeddings given by the
+                lens function. (default: :obj:`False`)
+            figsize (Tuple[int,int], optional):
+                The size of the visualization figure. (default: :obj:'(13, 11)')
+            node_size_scale(Tuple[int, int], optional):
+                The scale of node sizes. (default: :obj:`(100.0, 1000.0)`)
+            edge_width_scale(Tuple[int, int], optional):
+                The scale of edge widths. (default: :obj:`(1.0, 15.0)`)
+            legend_dict (dict, optional):
+                A :obj:`dict` associating to each node label its legend entry. Used only when using true labels.
+                (default: :obj:`None`)
+            discrete_colormap(bool, optional):
+                Setting this to :obj:`True` will use a discrete colormap for the node colors, instead of the
+                continuous colormap in the :math:`[0,1]` range. (default: :obj:`False`)
+            save_dir(str, optional):
+                Where to save the visualization image, if :obj:`None` the image is show instead. (deafault: :obj:`None`)
+            name(str, optional):
+                Custom file name for the visualization image. (deafault: :obj:`None`)
         """
 
         if use_true_labels:
@@ -114,6 +166,7 @@ class DGM:
 
         pull_back_cover = self._compute_pull_back_cover(data=data, nodes_embeddings=embeddings)
 
+        # Build the mapped graph from the pull back cover.
         mapped_graph = self._build_mapped_graph(data=data,
                                                 node_embeddings=embeddings,
                                                 pull_back_cover=pull_back_cover,
@@ -131,7 +184,16 @@ class DGM:
                          save_dir=save_dir,
                          name=name)
 
-    def _compute_embeddings(self, data: Data, random_state: int):
+    def _compute_embeddings(self, data: Data) -> np.ndarray:
+        """
+        Compute the node embeddings f(X) as the normalized output of the lens function.
+        A dimensionality reduction algorithm is used if the lens function is higher dimensional.
+        Args:
+            data(Data): The data for input graph.
+
+        Returns:
+            ndarray: the normalized embeddings
+        """
         embeddings = self.lens_function(data)
 
         # Reduce embeddings dimensionality
@@ -158,17 +220,15 @@ class DGM:
 
     def _compute_pull_back_cover(self, data: Data, nodes_embeddings: np.ndarray) -> List[np.ndarray]:
         r"""
-        Computes the pull back cover `nodes_embeddings*(U)` of the nodes of the input graph `X` with respect to the
-        intervals of the open cover `U` and lens function `nodes_embeddings`. Given the nodes embeddings
-        `nodes_embeddings(X)`, for each interval `U_i` the subset of nodes `X_i` belonging to the preimage of
-        `nodes_embeddings` with respect to that interval, `nodes_embeddings-1(U_i)`, is computed and the collection
-        of those subsets forms the pull back cover `nodes_embeddings*(U)`. See section 3.1 of
-        `https://arxiv.org/abs/2002.03864`_.
+        Computes the pull back cover f*(U) of the nodes of the input graph X with respect to the
+        intervals of the open cover `U` and lens function f. Given the nodes embeddings
+        f(X), for each interval U_i the subset of nodes X_i belonging to the preimage of
+        f(U_i), f-1(U_i), is computed and the collection of those subsets forms the pull back cover
+        f*(U)={f-1(U_i)}_i. See section 3.1 of https://arxiv.org/abs/2002.03864.
 
         Args:
-            data (Data): The data for the input graph `X`.
-            nodes_embeddings (ndarray): The embeddings `nodes_embeddings(X)` of the graph nodes produced by the lens
-            function `nodes_embeddings`.
+            data (Data): The data for the input graph X.
+            nodes_embeddings (ndarray): The embeddings f(X) of the graph nodes produced by the lens function.
 
         Returns:
             List[ndarray]: The pull back cover.
@@ -203,6 +263,20 @@ class DGM:
                             pull_back_cover: List[np.ndarray],
                             node_true_labels: Optional[np.ndarray] = None
                             ) -> nx.Graph:
+        """
+        Builds the mapped graph from the node embeddings and the corresponding pull back cover.
+        The mapped graph contains a node for each connected component in each subset in the pull back cover.
+        Edges are added to the mapped graph depending on the interconnectivity of those components.
+        Args:
+            data (Data): The graph data
+            node_embeddings (ndarray): The (normalized) embeddings.
+            pull_back_cover (List[ndarray]): Ther pull back cover
+            node_true_labels (bool):
+             If ture use the node true labels for determining the mapped graph node colors, instead of the embeddings.
+
+        Returns:
+            Graph: the mapped graph.
+        """
 
         original_graph: nx.Graph = to_networkx(data, to_undirected=True)
         mapped_graph = nx.Graph()
@@ -287,6 +361,18 @@ class DGM:
         return mapped_graph
 
     def _filter_small_components(self, mapped_graph: nx.Graph) -> nx.Graph:
+        """
+        Filters out from the mapped graph the connected components whose size is smaller than a threshold,
+        then prunes dangling edges. The size S_m(v) of a node v in the mapped graph is given by the number of nodes
+        contained in the connected component of the original graph that has been mapped to node v. The size of a
+        connected component in the mapped graph is the sum its nodes sizes.
+
+        Args:
+            mapped_graph (Graph): the mapped graph.
+
+        Returns:
+            Graph: the filtered mapped graph.
+        """
         filtered_graph = nx.Graph()
 
         # Boolean mask to filter out small nodes
@@ -349,6 +435,7 @@ class DGM:
                     save_dir: Optional[str] = None,
                     name: Optional[str] = None
                     ) -> None:
+
         assert not discrete_colormap or (self.num_dims == 1 or legend_dict is not None), \
             f"Discrete colormap not supported when using embeddings with more than one dimension"
 
@@ -435,7 +522,6 @@ class DGM:
 
     @staticmethod
     def _make_2d_color_map(values_x: np.ndarray, values_y: np.ndarray):
-
         values_x = np.array(
             (values_x - values_x.min()) / (values_x.max() - values_x.min()) * 255, dtype=np.int32)
         values_y = np.array(
